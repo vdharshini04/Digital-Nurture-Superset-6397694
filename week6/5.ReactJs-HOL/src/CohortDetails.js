db69fc9dd9806501c0c361e9fa2b67c43c6c6723
vdharshini04 <dhar22032.ec@rmkec.ac.in>
2025-07-27 22:13:22 +0530

    React HOL 5 uploaded

---
+++ week6/5.ReactJs-HOL/src/CohortDetails.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './CohortDetails.module.css';
+
+function CohortDetails(props) {
+    const status = props.cohort.currentStatus.toLowerCase(); // "ongoing" or "completed" or other
+    const headingStyle = {
+        color: status === "ongoing" ? "green" : "blue"
+    };
+
+    return (
+        <div className={styles.box}>
+            <h3 style={headingStyle}>
+                {props.cohort.cohortCode} - 
+                <span> {props.cohort.technology}</span>
+            </h3>
+            <dl>
+                <dt>Started On</dt>
+                <dd>{props.cohort.startDate}</dd>
+                <dt>Current Status</dt>
+                <dd>{props.cohort.currentStatus}</dd>
+                <dt>Coach</dt>
+                <dd>{props.cohort.coachName}</dd>
+                <dt>Trainer</dt>
+                <dd>{props.cohort.trainerName}</dd>
+            </dl>
+        </div>
+    );
+}
+
+export default CohortDetails;
